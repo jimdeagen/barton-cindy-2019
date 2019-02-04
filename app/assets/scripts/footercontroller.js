9031ee795e97dbc432845236468697d3d67b4c0b
Jim <jimdeagen@comcast.net>
2019-02-04 15:43:26 -0800

    Start improvements to media queries

---
+++ app/assets/scripts/footercontroller.js
@@ -0,0 +1,12 @@
+var myApp = angular.module("myApp", []);
+
+myApp.directive("myGreatFooter", function() {
+  var currentDate = new Date();
+  var currentYear = currentDate.getFullYear();
+  return {
+    template:
+      "<p>&#169; 2015-" +
+      currentYear +
+      ' reading and spelling success</p><p>This site built by <a href="http://www.jimdeagen.com" target="_blank">Jim Deagen</a></p></hr>'
+  };
+});
