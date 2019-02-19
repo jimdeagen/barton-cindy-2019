e4a55be23ac525c80ff714387811dbb63f1cefff
Jim <jimdeagen@comcast.net>
2019-02-19 14:51:04 -0800

    form submission fixing for modal thank you. In progress

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,7 +5,7 @@ import StickyHeader from "./modules/StickyHeader";
 import Modal from "./modules/Modal";
 
 var mobileMenu = new MobileMenu();
-new RevealOnScroll($(".feature-item"), "65%");
-new RevealOnScroll($(".testimonial"), "65%");
+new RevealOnScroll($(".feature-item"), "75%");
+new RevealOnScroll($(".testimonial"), "75%");
 var stickyHeader = new StickyHeader();
 var modal = new Modal();
