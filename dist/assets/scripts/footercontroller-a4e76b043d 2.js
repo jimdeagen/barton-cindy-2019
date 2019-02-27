a8d66c0c90ef8a8ee171bbfd340d4fedcfa86224
Jim <jimdeagen@comcast.net>
2019-02-27 10:30:28 -0800

    Upload review cycle version including build (dist) files.

---
+++ dist/assets/scripts/footercontroller-a4e76b043d 2.js
@@ -0,0 +1,1 @@
+var myApp=angular.module("myApp",[]);myApp.directive("myGreatFooter",function(){return{template:"<p>&#169; 2015-"+(new Date).getFullYear()+' reading and spelling success</p><p>This site built by <a href="http://www.jimdeagen.com" target="_blank" title="Web craft and development">Jim Deagen</a></p></hr>'}});
